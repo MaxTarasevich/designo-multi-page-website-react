b7a8dd8ed4e98c16b0202ef6aca8421503250c51
Max <maxtarasevich@gmail.com>
2022-08-13 20:32:45 +0300

    feat: add pages and reactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,34 @@ import React from 'react';
 
 import './App.scss';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import About from './pages/About';
+import AppDesign from './pages/AppDesign';
+import Contact from './pages/Contact';
+import GraphicDesign from './pages/GraphicDesign';
+import WebDesign from './pages/WebDesign';
+import Locations from './pages/Locations';
+
 function App() {
   return (
-    <div className="App">
-     <h1>
-      Hello
-     </h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+     <Route path='/' element={<Home />} />
+     <Route path='/about' element={<About />} />
+     <Route path='/app-design' element={<AppDesign />} />
+     <Route path='/contact' element={<Contact />} />
+     <Route path='/graphic-design' element={<GraphicDesign />} />
+     <Route path='/location' element={<Locations />} />
+     <Route path='/web-design' element={<WebDesign />} />
+       
+    </Routes>
+  </BrowserRouter>
   );
 }
 
